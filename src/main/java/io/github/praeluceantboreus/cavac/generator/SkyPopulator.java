42a719e4ddb8c8d2053b69bd6d3df1ca735e00d7
PraeluceantBoreus <richard.stoeckl@aon.at>
2015-07-27 18:51:02 +0200

    first generator

---
+++ src/main/java/io/github/praeluceantboreus/cavac/generator/SkyPopulator.java
@@ -0,0 +1,58 @@
+package io.github.praeluceantboreus.cavac.generator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.TreeType;
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
+
+public class SkyPopulator extends BlockPopulator
+{
+
+	private int xWidth, zWidth, height, begin;
+	private Material ground;
+	private ArrayList<TreeType> trees, originalTrees;
+
+	public SkyPopulator(int xWidth, int zWidth, int height, int begin, Material ground, ArrayList<TreeType> trees)
+	{
+		super();
+		this.xWidth = xWidth;
+		this.zWidth = zWidth;
+		this.height = height;
+		this.begin = begin;
+		this.ground = ground;
+		this.trees = new ArrayList<>(trees);
+		this.originalTrees = new ArrayList<>(trees);
+	}
+
+	@Override
+	public void populate(World arg0, Random arg1, Chunk arg2)
+	{
+		for (int x = 0; x < xWidth && x < arg2.getX() * 16 + 16; x++)
+		{
+			for (int z = 0; z < zWidth && z < arg2.getZ() * 16 + 16; z++)
+			{
+				for (int y = begin; begin - y < height; y--)
+				{
+					Location loc = new Location(arg0, x, y, z);
+					loc.getBlock().setType(ground);
+				}
+				if ((x == xWidth - 1 || x == 0) && (z == zWidth - 1 || z == 0))
+				{
+					if (trees.size() < 1)
+						trees = new ArrayList<>(originalTrees);
+					Collections.shuffle(trees, arg1);
+					TreeType type = trees.get(0);
+					trees.remove(0);
+					arg0.generateTree(new Location(arg0, x, begin + 1, z), type);
+				}
+			}
+		}
+	}
+
+}
--- src/main/java/io/github/praeluceantboreus/cavac/generator/VacuumGenerator.java
+++ src/main/java/io/github/praeluceantboreus/cavac/generator/VacuumGenerator.java
@@ -1,42 +1,59 @@
 package io.github.praeluceantboreus.cavac.generator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.TreeType;
 import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 
 public class VacuumGenerator extends ChunkGenerator
 {
-	@SuppressWarnings("deprecation")
+	private int xWidth, zWidth, height, begin;
+	private Material ground;
+	private ArrayList<TreeType> trees;
+
+	public VacuumGenerator(int xWidth, int zWidth, int height, int begin, Material ground)
+	{
+		super();
+		this.xWidth = xWidth;
+		this.zWidth = zWidth;
+		this.height = height;
+		this.begin = begin;
+		this.ground = ground;
+	}
+
 	@Override
 	public byte[] generate(World world, Random random, int x, int z)
 	{
-		// TODO Auto-generated method stub
-		return super.generate(world, random, x, z);
+		byte[] result = new byte[32768];
+		/*
+		 * for (int ix = 0; ix < x && ix < 16; ix++) { for (int iz = 0; iz < z
+		 * && iz < 16; iz++) { } }
+		 */
+		return result;
 	}
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public short[][] generateExtBlockSections(World world, Random random, int x, int z, BiomeGrid biomes)
+	public static int getBytePosition(int x, int y, int z)
 	{
-		// TODO Auto-generated method stub
-		return super.generateExtBlockSections(world, random, x, z, biomes);
+		return (x * 16 + z) * 128 + y;
 	}
 
 	@Override
 	public List<BlockPopulator> getDefaultPopulators(World world)
 	{
-		// TODO Auto-generated method stub
-		return super.getDefaultPopulators(world);
+		ArrayList<BlockPopulator> ret = new ArrayList<>();
+		ret.add(new SkyPopulator(xWidth, zWidth, height, begin, ground, trees));
+		return ret;
 	}
 
 	@Override
 	public Location getFixedSpawnLocation(World world, Random random)
 	{
-		// TODO Auto-generated method stub
-		return super.getFixedSpawnLocation(world, random);
+		return new Location(world, xWidth / 2, begin + 2, zWidth / 2);
 	}
 }
